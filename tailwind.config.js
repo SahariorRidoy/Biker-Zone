01751c10d8f9a52e51051388c9ae11d2ded7c188
Saharior Ridoy <emonhridoy2014@gmail.com>
2024-08-19 23:18:58 +0600

    Initial Setup and added tailwind daisy ui

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          
+        },
+      },
+    },
+    daisyui: {
+        themes: [
+          "light",
+          "dark",
+          "cupcake",
+          "bumblebee",
+          "emerald",
+          "corporate",
+          "synthwave",
+          "retro",
+          "cyberpunk",
+          "valentine",
+          "halloween",
+          "garden",
+          "forest",
+          "aqua",
+          "lofi",
+          "pastel",
+          "fantasy",
+          "wireframe",
+          "black",
+          "luxury",
+          "dracula",
+          "cmyk",
+          "autumn",
+          "business",
+          "acid",
+          "lemonade",
+          "night",
+          "coffee",
+          "winter",
+          "dim",
+          "nord",
+          "sunset",
+        ],
+      },
+    plugins: [require('daisyui')],
+  };

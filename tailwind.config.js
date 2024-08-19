8951c7df38da5b56232d1496998e8209923bb134
Saharior Ridoy <emonhridoy2014@gmail.com>
2024-08-20 01:27:08 +0600

    Added Latest News Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@
     
 
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'bike-primary': '#E76F51',
+          'bike-primary-bg': 'rgba(231, 111, 81, 0.1)',
+        },
+        boxShadow: {
+          'shadow-bike': '0px 0px 4px 0px rgba(0, 0, 0, 0.15)',
+        },
+      },
     },
+    variants: {},
     plugins: [],
   }
+  

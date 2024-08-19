2d07cab4cbf6cc1522ae4245cf941169cd89306b
Saharior Ridoy <emonhridoy2014@gmail.com>
2024-08-20 00:38:43 +0600

    Added Featurd Bikes Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,11 @@
+
+    
+
 /** @type {import('tailwindcss').Config} */
-// tailwind.config.js
 module.exports = {
+    content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {
-        colors: {
-          
-        },
-      },
+      extend: {},
     },
-    daisyui: {
-        themes: [
-          "light",
-          "dark",
-          "cupcake",
-          "bumblebee",
-          "emerald",
-          "corporate",
-          "synthwave",
-          "retro",
-          "cyberpunk",
-          "valentine",
-          "halloween",
-          "garden",
-          "forest",
-          "aqua",
-          "lofi",
-          "pastel",
-          "fantasy",
-          "wireframe",
-          "black",
-          "luxury",
-          "dracula",
-          "cmyk",
-          "autumn",
-          "business",
-          "acid",
-          "lemonade",
-          "night",
-          "coffee",
-          "winter",
-          "dim",
-          "nord",
-          "sunset",
-        ],
-      },
-    plugins: [require('daisyui')],
-  };
+    plugins: [],
+  }
